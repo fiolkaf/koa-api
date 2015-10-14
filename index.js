846eab2e8ba08ad41a61f1ce1a8c2282fab78f51
Filip Fiolka <fiolkaf@gmail.com>
2015-10-14 18:38:41 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-modules('.').routes()
-    .apply(koa())
-    .on('request:start', console.log.apply(msg))
-    .on('request:end', console.log.apply(err))
-    .on('error', koa.release);
+koaApi()
+    .on('request:start', console.log)
+    .on('request:end', console.log)
+    .on('error', console.error)
+    .routes(modules('./api/v1'))
+    .routes(modules('./api/v2'))
+    .log('trace');
